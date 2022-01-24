e0ed0cb2a451a8389ed7849d6f1b9de7f55a24ee
Sanghyeon Park <sh.austin.park@gmail.com>
2022-01-24 17:47:17 +0900

    Added cell-type specific analysis result processing

--- README.md
+++ README.md
@@ -21,3 +21,26 @@ Rscript ./process_parh2.R --file ./example/parh2.example.results --dir ./example
 - Output
     ![Result](./example/example.result.png)
 
+
+# Cell-type Specific Analysis Result Processing
+
+Processing of cell-type specific analysis result obtained by using LDSC tool.
+
+Version:
+- v1 : Calculate P-value of FDR. This P-value is added to the result as Coefficient_P_FDR.
+
+
+Processed result file will be saved as `*_withFDR.csv`.
+
+## Example
+```
+Rscript ./process_ctanalysis.R --file ./example/example_CT_Cahoy.cell_type_results.txt --dir ./example/
+```
+
+- Options
+    - `--file <filename>` : Result file path of cell-type specific analysis.
+    - `--dir <directory>` : Directory path where the FDR calculated result file will be saved. If not specified, it will be saved in the same directory as where the Rscript is present.
+
+
+- Output
+    ![Result](./example/example_CT_Cahoy.cell_type_results_withFDR.png)
--- process_ctanalysis.R
+++ process_ctanalysis.R
@@ -1,5 +1,3 @@
-# Partitioned heritability result processing
-
 suppressPackageStartupMessages({
   library(data.table)
   library(stringr)
@@ -8,20 +6,19 @@ suppressPackageStartupMessages({
 
 
 parser <- argparse::ArgumentParser(description=":: FDR calculation for Paritioned h2 result; by Sanghyeon Park. ::", formatter_class="argparse.ArgumentDefaultsHelpFormatter")
-parser$add_argument("--file", "-f", required=TRUE, help="Result file path of partitioned h2.")
+parser$add_argument("--file", "-f", required=TRUE, help="Result file path of partitioned h2 cell-type specific analysis.")
 parser$add_argument("--dir", required=FALSE, default='', help="Directory path where the FDR calculated result file will be saved.")
 
 args <- parser$parse_args()
 
-partitioned_h2_processing <- function(FILE, save2='') {
-  filename <- str_split(FILE, "/")
-  filename <- filename[[1]][length(filename[[1]])]
+celltype_analysis_processing <- function(FILE, save2='') {
+  sp <- str_split(FILE, "/")
+  filename <- str_sub(sp[[1]][length(sp[[1]])], end=-5)
   data <- fread(file=FILE, header=TRUE)
-  p <- 2*pnorm(-abs(data$'Coefficient_z-score'))
-  cat('Number of genes compared:', length(p), '\n')
+  p <- data$Coefficient_P_value
+  cat('Number of cell types compared:', length(p), '\n')
   fdr <- p.adjust(p, method='fdr', n = length(p))
-  data$Cofficient_P <- p
-  data$Cofficient_P_FDR <- fdr
+  data$Coefficient_P_FDR <- fdr
   if(nchar(save2) == 0){
     save2 <- paste(getwd(), "/", sep="")
   }
@@ -33,4 +30,4 @@ partitioned_h2_processing <- function(FILE, save2='') {
   write.csv(data, paste(save2, paste(filename, "_withFDR.csv", sep=""), sep=""), row.names=TRUE)
 }
 
-partitioned_h2_processing(FILE=args$file, save2=args$dir)
+celltype_analysis_processing(args$file, args$dir)
